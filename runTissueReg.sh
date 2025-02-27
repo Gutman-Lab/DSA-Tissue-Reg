b436c48141bca42a7de88d9976242a0164a9645b
dgutman <dagutman@gmail.com>
2025-02-27 15:11:52 -0500

    forced platform to linuxamd64

--- runTissueReg.sh
+++ runTissueReg.sh
@@ -2,5 +2,5 @@
 ## This will rebuild the container, and start the app, and copy/bind the current working directory into /app for quick reload
 docker stop tissue-reg
 docker rm tissue-reg
-docker build -t tissue-reg .
+docker build --platform=linux/amd64 -t tissue-reg .
 docker run -it -p 8050:8050 -v $(pwd):/app --name tissue-reg tissue-reg /bin/bash
